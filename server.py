f495251f25489d9fd12b2845695037f567d31013
Alex Khilko <alex.khilko@tripledotstudios.com>
2024-01-16 16:22:56 +0100

    Add multiple connections support with threads

--- server.py
+++ server.py
@@ -3,9 +3,9 @@
 import socket
 from resp.parsers import serialize
 from commands import handle_request
-import time
 from exceptions import RedisServerException
 import logging
+import threading
 
 logger = logging.getLogger(__name__)
 
@@ -21,20 +21,14 @@ def _encode_error_response(error) -> bytes:
     return serialize([error], is_error=True).encode("utf-8")
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as lsock:
-    # Avoid bind() exception: OSError: [Errno 48] Address already in use
-    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    lsock.bind((HOST, PORT))
-    lsock.listen()
-    conn, addr = lsock.accept()
-    with conn:
+def handle_client(connection):
+    with connection:
         while True:
-            print(f"Connected by {addr}")
-            data = conn.recv(1024)
+            print(f"Connected by {address}")
+            data = connection.recv(1024)
             print(f"received data {data}")
             if not data:
-                time.sleep(2)
-                continue
+                break
             try:
                 response = handle_request(data)
             except RedisServerException as exc:
@@ -42,4 +36,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as lsock:
                 response = _encode_error_response(str(exc))
             else:
                 response = _encode_response(response)
-            conn.sendall(response)
+            connection.sendall(response)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as lsock:
+    # Avoid bind() exception: OSError: [Errno 48] Address already in use
+    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    lsock.bind((HOST, PORT))
+    lsock.listen()
+    while True:
+        client_socket, address = lsock.accept()
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        client_handler.start()
